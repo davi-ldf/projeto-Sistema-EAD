5168c51cd0f14c3cf5bf2bc19bd475cc1f881e29
Davi Lisboa <129395367+davi-ldf@users.noreply.github.com>
2024-06-09 23:19:47 -0300

    update pages

---
+++ pages/gerenciar_cursos.php
@@ -0,0 +1,64 @@
+<!-- Page-header start -->
+<div class="page-header card">
+    <div class="row align-items-end">
+        <div class="col-lg-8">
+            <div class="page-header-title">
+                <div class="d-inline">
+                    <h4>Gerenciar</h4>
+                    <span>Aqui estão todos os cursos cadastrados na plataforma.</span>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="page-header-breadcrumb">
+                <ul class="breadcrumb-title">
+                    <li class="breadcrumb-item">
+                        <a href="index.php">
+                            <i class="icofont icofont-home"></i>
+                        </a>
+                    </li>
+                    <li class="breadcrumb-item"><a href="#!">Gerenciar Cursos</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Page-header end -->
+
+<div class="page-body">
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="card">
+                <div class="card-header">
+                    <h5>Todos os Cursos</h5>
+                    <span><a href="index.php?p=cadastrar_curso">Clique aqui </a>para cadastrar um curso.</span>
+                </div>
+                <div class="card-block table-border style">
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Imagem</th>
+                                    <th>Título</th>
+                                    <th>Preço</th>
+                                    <th>Botões</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">1</th>
+                                    <td>Mark</td>
+                                    <td>Otto</td>
+                                    <td>@mdo</td>
+                                    <td>@mdo</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ pages/loja_cursos.php
@@ -0,0 +1,46 @@
+<!-- Page-header start -->
+<div class="page-header card">
+    <div class="row align-items-end">
+        <div class="col-lg-8">
+            <div class="page-header-title">
+                <div class="d-inline">
+                    <h4>Loja de Cursos</h4>
+                    <span>Adquira nossos cursos utilizando os seus créditos.</span>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="page-header-breadcrumb">
+                <ul class="breadcrumb-title">
+                    <li class="breadcrumb-item">
+                        <a href="index.php">
+                            <i class="icofont icofont-home"></i>
+                        </a>
+                    </li>
+                    <li class="breadcrumb-item">Loja de Cursos
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Page-header end -->
+
+<div class="page-body">
+    <div class="row">
+        <div class="col-sm-4">
+            <div class="card">
+                <div class="card-header">
+                    <h5>Cursos</h5>
+                </div>
+                <div class="card-block">
+                    <img src="https://cdn.falauniversidades.com.br/wp-content/uploads/2020/04/02122231/ead-1.jpg" class="img-fluid mb-3" alt="">
+                    <p>
+                        Aqui na plataforma EAD da Harvard University, são ofertados uma diversidade de cursos online acessíveis, práticos e completos para nossos usuários.
+                    </p>
+                    <butto class="btn form-control btn-out-dashed btn-success btn-square">Adquirir - $45</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
