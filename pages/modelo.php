01bcbcf6423f8c2f5f12a3723d9508cb0b2a582f
Davi Lisboa <129395367+davi-ldf@users.noreply.github.com>
2024-06-09 23:19:09 -0300

    Create modelo.php

---
+++ pages/modelo.php
@@ -0,0 +1,45 @@
+<!-- Page-header start -->
+<div class="page-header card">
+    <div class="row align-items-end">
+        <div class="col-lg-8">
+            <div class="page-header-title">
+                <div class="d-inline">
+                    <h4>Sample Page</h4>
+                    <span>lorem ipsum dolor sit amet, consectetur adipisicing elit</span>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="page-header-breadcrumb">
+                <ul class="breadcrumb-title">
+                    <li class="breadcrumb-item">
+                        <a href="index.php">
+                            <i class="icofont icofont-home"></i>
+                        </a>
+                    </li>
+                    <li class="breadcrumb-item"><a href="#!">Pages</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Page-header end -->
+
+<div class="page-body">
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="card">
+                <div class="card-header">
+                    <h5>Página Incial</h5>
+                    <span>Seja bem-vindo à Harvard University.</span>
+                </div>
+                <div class="card-block">
+                    <p>
+                        Aqui na plataforma EAD da Harvard University, são ofertados uma diversidade de cursos online acessíveis, práticos e completos para nossos usuários.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
