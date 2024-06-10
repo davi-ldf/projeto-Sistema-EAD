12075c257b18d55923fff4f9b152dd4df52c06a3
Davi Lisboa <129395367+davi-ldf@users.noreply.github.com>
2024-06-09 23:18:45 -0300

    Create inicial.php

---
+++ pages/inicial.php
@@ -0,0 +1,18 @@
+<div class="page-body">
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="card">
+                <div class="card-header">
+                    <h5>Página Incial</h5>
+                    <span>Seja bem-vindo à Harvard University.</span>
+                </div>
+                <div class="card-block">
+                    <p>
+                        Aqui na plataforma de EAD da Harvard University, são ofertados uma diversidade de cursos online acessíveis, práticos e completos para nossos usuários.
+                    </p>
+                    <p><a href="index.php?p=loja_cursos">Clique aqui para conhecer os nossos cursos</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
