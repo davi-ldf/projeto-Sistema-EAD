9f12a49d157c54ec820e186dd0d536b6094cdece
Davi Lisboa <129395367+davi-ldf@users.noreply.github.com>
2024-06-10 21:52:16 -0300

    Create config.example.php

---
+++ lib/config.example.php
@@ -0,0 +1,17 @@
+<?php 
+
+$host = 'localhost';
+$user = 'your_user';
+$pass = 'your_password';
+$db = 'escola_ead';
+// Crie o banco de dados acima e importe o arquivo SQL disponibilizado no diretório "database/".
+
+$mysqli = new mysqli($host, $user, $pass, $db);
+
+/* Check connection */
+if($mysqli->$connect_errno) {
+    echo "Connect failed: ". $mysqli->connect_error;
+    exit();
+}
+
+?>
