545c752c63efd4e918fd866594224ab9e5359aed
Davi Lisboa <129395367+davi-ldf@users.noreply.github.com>
2024-06-10 23:12:59 -0300

    Update cadastrar_curso.php

--- pages/cadastrar_curso.php
+++ pages/cadastrar_curso.php
@@ -1,3 +1,35 @@
+<?php
+include('lib/config.php');
+if(isset($_POST['enviar'])) {
+    $titulo = $_POST['titulo'];
+    $descricao_curta = $_POST['descricao_curta'];
+    $preco = $_POST['preco'];
+    $conteudo = $_POST['conteudo'];
+
+    $erro = array();
+    if(empty($titulo))
+        $erro[] = "Preencha o título";
+
+    if(empty($descricao_curta))
+        $erro[] = "Preencha a descrição curta";
+
+    if(empty($preco)) 
+        $erro[] = "Preencha o preço";
+
+    if(empty($conteudo)) 
+        $erro[] = "Preencha o conteúdo";
+
+    if(!isset($_FILES) || !isset($_FILES['imagem']) || $_FILES['size'] == 0)
+        $erro[] = "Selecione uma imagem para o conteúdo";
+    
+    if(count($erro) == 0) {
+
+    }
+
+    
+}
+
+?>
 <!-- Page-header start -->
 <div class="page-header card">
     <div class="row align-items-end">
@@ -29,47 +61,56 @@
 <div class="page-body">
     <div class="row">
         <div class="col-sm-12">
+        <?php if(isset($erro) && count($erro) > 0) {?> 
+        <div class="alert alert-danger" role="alert">
+            <?php foreach($erro as $e) { echo "$e<br>"; }?>
+        </div>
+        <?php 
+        }
+        ?>
             <div class="card">
                 <div class="card-header">
                     <h5>Formulário de Cadastro</h5>
                 </div>
                 <div class="card-block">
-                    <div class="row">
-                        <div class="col-lg-4">
-                            <div class="form-group">
-                                <label for="">Título</label>
-                                <input class="form-control" name="titulo" type="text">
+                    <form action="" method="post" enctype="multipart/form-data">
+                        <div class="row">
+                            <div class="col-lg-4">
+                                <div class="form-group">
+                                    <label for="">Título</label>
+                                    <input class="form-control" name="titulo" type="text">
+                                </div>
+                            </div>
+                            <div class="col-lg-8">
+                                <div class="form-group">
+                                    <label for="">Descrição curta</label>
+                                    <input class="form-control" name="descricao_curta" type="text">
+                                </div>
+                            </div>
+                            <div class="col-lg-8">
+                                <div class="form-group">
+                                    <label for="">Imagem</label>
+                                    <input class="form-control" name="imagem" type="file">
+                                </div>
+                            </div>
+                            <div class="col-lg-4">
+                                <div class="form-group">
+                                    <label for="">Preço</label>
+                                    <input class="form-control" name="preco" type="text">
+                                </div>
+                            </div>
+                            <div class="col-lg-12">
+                                <div class="form-group">
+                                    <label for="">Conteúdo</label>
+                                    <textarea class="form-control" rows="10" name="conteudo"></textarea>
+                                </div>
+                            </div>
+                            <div class="col-lg-12">
+                                <a href="index.php" class="btn btn-primary btn-round"> <i class="ti-arrow-left"></i>Voltar</a>
+                                <button type="submit" name="enviar" class="btn btn-success btn-round float-right"> <i class="ti-save"></i>Salvar</button>
                             </div>
                         </div>
-                        <div class="col-lg-8">
-                            <div class="form-group">
-                                <label for="">Descrição curta</label>
-                                <input class="form-control" name="descricao_curta" type="text">
-                            </div>
-                        </div>
-                        <div class="col-lg-8">
-                            <div class="form-group">
-                                <label for="">Imagem</label>
-                                <input class="form-control" name="imagem" type="file">
-                            </div>
-                        </div>
-                        <div class="col-lg-4">
-                            <div class="form-group">
-                                <label for="">Preço</label>
-                                <input class="form-control" name="preco" type="text">
-                            </div>
-                        </div>
-                        <div class="col-lg-12">
-                            <div class="form-group">
-                                <label for="">Conteúdo</label>
-                                <textarea class="form-control" rows="10" name="conteudo"></textarea>
-                            </div>
-                        </div>
-                        <div class="col-lg-12">
-                            <button class="btn btn-primary btn-round"> <i class="ti-arrow-left"></i>Voltar</button>
-                            <button class="btn btn-success btn-round float-right"> <i class="ti-save"></i>Salvar</button>
-                        </div>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
