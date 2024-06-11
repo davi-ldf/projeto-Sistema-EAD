a0a190d8a20cf1fb9cd0d6c0be6094ea5de44d7f
Davi Lisboa <129395367+davi-ldf@users.noreply.github.com>
2024-06-10 21:53:18 -0300

    Update config.example.php

--- lib/config.example.php
+++ lib/config.example.php
@@ -1,10 +1,11 @@
 <?php 
+//Renomeie este arquivo para 'config.php'
 
 $host = 'localhost';
 $user = 'your_user';
 $pass = 'your_password';
 $db = 'escola_ead';
-// Crie o banco de dados acima e importe o arquivo SQL disponibilizado no diretório "database/".
+//Crie o banco de dados acima e importe o arquivo SQL disponibilizado no diretório "database/".
 
 $mysqli = new mysqli($host, $user, $pass, $db);
 
