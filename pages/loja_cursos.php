b9a77597694ba06fbed97438109397e84d567321
Davi Lisboa <129395367+davi-ldf@users.noreply.github.com>
2024-06-15 15:49:21 -0300

    Update loja_cursos.php

--- pages/loja_cursos.php
+++ pages/loja_cursos.php
@@ -1,3 +1,46 @@
+<?php 
+
+include('lib/protect.php');
+protect(0);
+
+if(!isset($_SESSION))
+    session_start();
+
+$erro = false;
+if(isset($_POST['adquirir'])) {
+    //verify if the user is able to buy
+    $id_user = $_SESSION['usuario'];
+    $sql_query_creditos = $mysqli->query("SELECT creditos FROM usuarios WHERE id = '$id_user'") or die($mysqli->error); //pick user credits
+    $usuario = $sql_query_creditos->fetch_assoc();
+
+
+    $creditos_do_usuario = $usuario['creditos'];
+
+    $id_curso = intval($_POST['adquirir']);
+    $sql_query_curso = $mysqli->query("SELECT preco FROM cursos WHERE id = '$id_curso'") or die($mysqli->error); //pick course price
+    $curso = $sql_query_curso->fetch_assoc();
+
+    $preco_do_curso = $curso['preco'];
+
+    if($preco_do_curso > $creditos_do_usuario) {
+        $erro = "Você não possui créditos para adquirir este curso.";
+    } else {
+        $mysqli->query("INSERT INTO relatorio (id_usuario, id_curso, valor, data_compra) VALUES(
+            '$id_user',
+            '$id_curso',
+            '$preco_do_curso',
+            NOW()
+        )") or die($mysqli->error);
+        $novo_credito = $creditos_do_usuario - $preco_do_curso;
+        $mysqli->query("UPDATE usuarios SET creditos = '$novo_credito' WHERE id = '$id_user'") or die($mysqli->error);
+        die("<script>location.href='index.php?p=meus_cursos';</script>");
+    }
+
+}
+
+$id_usuario = $_SESSION['usuario'];
+$cursos_query = $mysqli->query("SELECT * FROM cursos WHERE id NOT IN (SELECT id_curso FROM relatorio WHERE id_usuario = '$id_usuario')") or die($mysqli->error);
+?>
 <!-- Page-header start -->
 <div class="page-header card">
     <div class="row align-items-end">
@@ -28,19 +71,32 @@
 
 <div class="page-body">
     <div class="row">
+        <div class="col-sm-12">
+            <?php if($erro !== false) {?> 
+            <div class="alert alert-danger" role="alert">
+                <?php echo $erro; ?>
+            </div>
+            <?php 
+            }
+            ?>
+        </div>
+        <?php while($curso = $cursos_query->fetch_assoc()) {?>
         <div class="col-sm-4">
             <div class="card">
                 <div class="card-header">
-                    <h5>Cursos</h5>
+                    <h5><?php echo $curso['titulo']; ?></h5>
                 </div>
                 <div class="card-block">
-                    <img src="https://cdn.falauniversidades.com.br/wp-content/uploads/2020/04/02122231/ead-1.jpg" class="img-fluid mb-3" alt="">
+                    <img src="<?php echo $curso['imagem']; ?>" class="img-fluid mb-3" alt="">
                     <p>
-                        Aqui na plataforma EAD da Harvard University, são ofertados uma diversidade de cursos online acessíveis, práticos e completos para nossos usuários.
+                    <?php echo $curso['descricao_curta']; ?>
                     </p>
-                    <butto class="btn form-control btn-out-dashed btn-success btn-square">Adquirir - $45</button>
+                    <form action="" method="post">
+                    <button name="adquirir" type="submit" value="<?php echo $curso['id']; ?>" class="btn form-control btn-out-dashed btn-success btn-square">Adquirir - $<?php echo number_format($curso['preco'], 2, ',', '.'); ?></button>
+                    </form>
                 </div>
             </div>
         </div>
+        <?php }?>
     </div>
 </div>
